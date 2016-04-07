7cdc83ca15a580f7911d31ac3b94c931d8262b11
FutureChen <Future_C>
2016-04-07 18:22:28 +0800

    Initial Commit

---
+++ view/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  view
+//
+//  Created by 张晓军 on 16/4/7.
+//  Copyright © 2016年 枫叶砂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
